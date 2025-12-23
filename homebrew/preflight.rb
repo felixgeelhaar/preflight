4cee0a7ce671a021361ceef7239a3c1950998d8e
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-23 12:35:06 +0100

    chore(homebrew): update formula with v0.1.0 SHA256 hashes

--- homebrew/preflight.rb
+++ homebrew/preflight.rb
@@ -9,20 +9,20 @@ class Preflight < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/felixgeelhaar/preflight/releases/download/v#{version}/preflight-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "f93cac37438e5273b713d9a929dac7d987bfa80578f588c7e24f93b651d349f2"
     else
       url "https://github.com/felixgeelhaar/preflight/releases/download/v#{version}/preflight-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "05c8216b2f8896977d3528b88cd0a6e42f5cb1e25098ab0aba91cc217f2b5c62"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/felixgeelhaar/preflight/releases/download/v#{version}/preflight-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "eb1576fc05b8c5b3136b73319661431dda5dc4f4a03fad2d9c6cc7ae237dba03"
     else
       url "https://github.com/felixgeelhaar/preflight/releases/download/v#{version}/preflight-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "263dca38929c73c86f9f0650d3bdd6946862267db40f055e08ff914dba3ec536"
     end
   end
 
